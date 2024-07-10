665675f7855a94b7a4d1026a35892a6741a6f03c
NiharaAz <niharaazeem@gmail.com>
2024-07-10 17:17:35 +0800

    'new'

--- kafka_test/src/main/java/com/example/kafka_test/DemoApplication.java
+++ kafka_test/src/main/java/com/example/kafka_test/DemoApplication.java
@@ -3,11 +3,21 @@ package com.example.kafka_test;
 import com.example.kafka_test.data.Itinerary_Info;
 import com.example.kafka_test.data.mmbs_Ref_Info;
 import com.example.kafka_test.data.traveller_Info;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaderMapper;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @ShellComponent
@@ -23,7 +33,18 @@ public class DemoApplication  {
     public void sendMessage(){
         ICS_data data =SetData();
 
-        kafkaTemplate.send(Topic,data);
+        //kafkaTemplate.send(Topic,data);
+        List<Header> headers= new ArrayList<>();
+        headers.add(new RecordHeader("msgActCd", "A".getBytes()));
+        headers.add(new RecordHeader("msgCreateDatetime","2020-08-24T14:15:22+08:00".getBytes()));
+        
+
+        var record= new ProducerRecord<String, ICS_data>(Topic,data);
+        record.headers().add("msgActCd", "A".getBytes());
+        record.headers().add("msgCreateDatetime","2020-08-24T14:15:22+08:00".getBytes());
+        kafkaTemplate.send(record);
+
+        //kafkaTemplate.send()
     }
 
     private ICS_data SetData() {
